aafd9c89a1b081a1e6cf4fed0718f601f69d2d00
Adarsh Dadwal <133523790+adarsh0052@users.noreply.github.com>
2026-06-03 12:58:12 +0530

    Time: 37 ms (96.68%) | Memory: 29.2 MB (44.78%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+       vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
         int n = nums.size();
-        vector<vector<int>> result;
-        for (int first = 0; first < n - 2; first++) {
-            if (first > 0 && nums[first] == nums[first - 1]) continue;
-            int left = first + 1;
-            int right = n - 1;
-            while (left < right) {
-                int curr_sum = nums[first] + nums[left] + nums[right];
-                if (curr_sum == 0) {
-                    result.push_back({nums[first], nums[left], nums[right]});
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-                    left++;
-                    right--;
-                } else if (curr_sum < 0) {
-                    left++;
-                } else {
-                    right--;
-                }
+        for(int i=0;i<n-2;i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            int st = i+1, ed = n-1, sum = -1 * nums[i];
+            while(st<ed){
+                int s = nums[st] + nums[ed];
+                if(s == sum){
+                    ans.push_back({nums[i], nums[st], nums[ed]});
+                    st++;
+                    ed--;
+                    while(st<ed && nums[st]==nums[st-1]){
+                        st++;
+                    }
+                    while(st<ed && nums[ed]==nums[ed+1]){
+                        ed--;
+                    }}
+                else if(s<sum){
+                        st++;
+                    }
+                else{
+                        ed--;
+                    }
             }
         }
-        return result;
+
+        return ans;
+        
     }
 };
